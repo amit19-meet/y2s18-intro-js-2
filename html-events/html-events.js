169c7d91c53e0d61e190e670a1f39a14f3d9a2a2
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 18:36:53 +0300

    day 4

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,8 @@
 // Put all your JavaScript in this file!
+function favColor() {
+	var answer= prompt("What's your favorite color?");
+	var change= document.getElementsByTagName("body");
+	change[0].style.backgroundColor= answer;
+
+
+}
--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,10 @@
+var me= {
+	firstName: "Amit",
+	lastName: "Horowitz",
+	favoriteFoods: ["lasagna", "frozen mango"],
+	age: 16,
+	picture: "https://cdn.akc.org/Marketplace/Breeds/Golden_Retriever_SERP.jpg",
+
+}
 // Create your 'me' object literal here!
+document.write(JSON.stringify(me))
